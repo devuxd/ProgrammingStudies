87a272df1269375b4aa4c82d486418b7a4799eac
wave-inguane <avelinoinguane@hotmail.com>
2017-02-20 20:07:53 -0500

    css update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Created by tlatoza on 11/23/15.
  * updated by Wave Inguane on 02/20/2017.
  */
+
 "use strict";
 
 var express = require('express');
@@ -13,32 +14,40 @@ var pastebinURL = 'https://seecoderun.firebaseapp.com/#-';
 var nextSession;             // JSON structure for the next session
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('client'))
+app.use(express.static('client'));
 app.set('port',(process.env.PORT || 8888));
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+
+//.................................................................................
 //Admin action to initialize a new study by creating a set of workflows.
+//.................................................................................
 app.get('/createWorkflows', function (req, res) {
     createWorkflows();
     res.sendFile(__dirname + '/client/successCreatingWorkflows.html');
 });
 
+//.................................................................................
 //TODO: draft a disclaimer
 // Do we keep a record that the participant agreed to the terms of usage
 // or we just let them proceed to the screening task after they have accepted
 // the terms and conditions to participate ?
 // What if they don't click Accept button ?
+//.................................................................................
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/client/welcome.html');
 });
 
-// When a user first hits the study server, first check if they have already participated.
-// If not, send them to the screening test.
+//.................................................................................
+// When a user first hits the study server, first check if they have already
+// participated. If not, send them to the screening test.
+//.................................................................................
 app.get('/screeningTask', function (req, res) {
     var workerId = req.query.workerId;
 
-    // Check if there is already data for this worker in Firebase. If there is, the worker has already participated.
+    // Check if there is already data for this worker in Firebase.
+    // If there is, the worker has already participated.
     var workerRef = new Firebase(firebaseStudyURL + '/workers/' + workerId);
     workerRef.once('value', function(snapshot) {
         if (snapshot.val() == null)
@@ -46,10 +55,12 @@ app.get('/screeningTask', function (req, res) {
             // TODO: We need to be able to send back to the client the id of the worker.
             // Options: (1) we could use a templating engine to embed it in the code.
             // Probably need to set up one of the mustache processors.
-            // We could also then use templating to set up worker specific content in the templates.
-            // Like customizing the instructions. Or hiding or showing the chat system for different versions.
+            // We could also then use templating to set up worker specific content in
+            // the templates.Like customizing the instructions. Or hiding or showing
+            // the chat system for different versions.
 
-            res.sendFile(__dirname + '/client/screening.html');///?workerId=5'); // ?workerId=' + workerId);
+            res.sendFile(__dirname + '/client/screening.html');///?workerId=5');
+            // ?workerId=' + workerId);
         }
         else
         {
@@ -58,28 +69,37 @@ app.get('/screeningTask', function (req, res) {
     });
 });
 
-// After finishing the screening, check if they passed. If so, send them to the demographics page.
+//.................................................................................
+// After finishing the screening, check if they passed. If so, send them to the
+// demographics page.
+//.................................................................................
 app.post('/screenSubmit', function (req, res) {
     console.log('screening submitted'); 
     console.log(req.body.question1 + " " + req.body.taskTimeMillis);
     var result = req.body.question1;
     if(result == 7)
+        //TODO: Update the demographic collection form
+        //TODO: Wave see feedback email 2/20/2017
         res.sendFile(__dirname + '/client/demographics.html');
     else
         res.sendFile(__dirname + '/client/failedScreening.html');
 
 });
 
-
+//.................................................................................
 // After finishing the demographics survey, send the user to the waiting room.
+//.................................................................................
 app.post('/waitingroom', function (req, res) {
-    // TODO: store the demographics data to firebase, associated with the participant. // what specific demographic do we send?
+    // TODO: store the demographics data to firebase, associated with the participant.
+    // TODO: Wave see feedback email 2/20/2017
+    // TODO: Next actual task page Single or Group session ?
       res.sendFile(__dirname + '/client/waitingRoom.html');
 });
 
 
-
+//.................................................................................
 // Start the server.
+//.................................................................................
 var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
     var port = server.address().port;
@@ -88,12 +108,17 @@ var server = app.listen(app.get('port'), function () {
     console.log('http://localhost:' + port + '/');
 });
 
+
+//.................................................................................
+//
+//.................................................................................
 function firebaseSetup()
 {
     var waitListRef = new Firebase(firebaseStudyURL + '/waitlist');
 
-    // Watch for additions to the waitingList. Once the waiting list meets or exceeds the size of the
-    // number of participants required for the nextSession, start the session.
+    // Watch for additions to the waitingList. Once the waiting list meets or
+    // exceeds the size of the number of participants required for the nextSession,
+    // start the session.
     waitListRef.on("value", function(snapshot) {
         if (nextSession != null)
         {
@@ -119,7 +144,10 @@ function firebaseSetup()
     });
 }
 
-// Create the workflows on Firebase and the corresponding initial sessions for each workflow.
+//.................................................................................
+// Create the workflows on Firebase and the corresponding initial sessions for each
+//workflow.
+//.................................................................................
 function createWorkflows()
 {
     var totalWorkflowCount = 2;
@@ -127,7 +155,8 @@ function createWorkflows()
     var workflows = {};
     var sessions = {};
 
-    // Create a JSON object for each workflow and a corresponding first session for each workflow
+    // Create a JSON object for each workflow and a corresponding first session for
+    // each workflow
     for (var i=0; i < totalWorkflowCount; i++) {
         var workflow = {};
         workflow.workflowURL = pastebinURL + 'workflowAAAA' + i;
@@ -146,7 +175,8 @@ function createWorkflows()
         sessions[workflowID] = session;
     }
 
-    // Create the initial status, setting up the first session and the current total number of sessions.
+    // Create the initial status, setting up the first session and the current total
+    // number of sessions.
     var status = {};
     status.nextSessionID = 0;
     status.totalSessions = totalWorkflowCount;
@@ -163,7 +193,10 @@ function createWorkflows()
     nextSession = sessions[0];
 }
 
+
+//.................................................................................
 // Starts the corresponding session.
+//.................................................................................
 function startSession(session, waitlistSnapshot)
 {
     // Load status
@@ -174,7 +207,8 @@ function startSession(session, waitlistSnapshot)
         {
             var status = snapshot.val();
 
-            // Build the list of IDs for the workers who will be working on this session.
+            // Build the list of IDs for the workers who will be working on this
+            // ession.
             var workers = {};
             var i = 0;
             waitlistSnapshot.forEach(function(waitlistEntrySnapshot) {
@@ -201,13 +235,14 @@ function startSession(session, waitlistSnapshot)
             var activeSessionsRef = new Firebase(firebaseStudyURL + '/status/activeSessions');
             activeSessionsRef.push(session.sessionID);
 
-            // Set the sessionURL for each of the first totalParticipants workers on the waitlist.
-            // Setting this URL will cause each of these worker clients that are currently waiting to join the session.
+            // Set the sessionURL for each of the first totalParticipants workers
+            // on the waitlist. Setting this URL will cause each of these worker
+            // clients that are currently waiting to join the session.
             // We do this last to ensure that the session is now fully set up.
             i = 0;
             waitlistSnapshot.forEach(function(waitlistEntrySnapshot) {
-                var workerWaitlistRef = new Firebase(firebaseStudyURL + '/waitlist/' + waitlistEntrySnapshot.key() +
-                    '/sessionURL');
+                var workerWaitlistRef = new Firebase(firebaseStudyURL + '/waitlist/' +
+                    waitlistEntrySnapshot.key() + '/sessionURL');
                 workerWaitlistRef.set(session.workflowURL);
 
                 i++;
@@ -215,7 +250,6 @@ function startSession(session, waitlistSnapshot)
                 if (i >= session.totalParticipants)
                     return true;    // break
             });
-             
 
             // TODO: Set a timeout to be able to end the session when the time is up???
             //set a timer, end it even if submit is not clicked
@@ -224,7 +258,9 @@ function startSession(session, waitlistSnapshot)
     });
 }
 
+//.................................................................................
 // To be called when a session has been finished.
+//.................................................................................
 function sessionCompleted(sessionID) // update Firebase 
 {
     // Add the sessionID that was just finished --> Add to where? Firebase? Or should I create another array? 
